757906fff59e6bca647ae2772d08019a69cac721
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-09-06 22:28:01 -0400

    This is all a bit ambitious but let's see. Gonna attempt an ASP.NET backend with a js discord bot.

---
+++ FlawsFightNightServer.Core/Entities/Tournament.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlawsFightNightServer.Core.Entities
+{
+    public class Tournament
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Team> Teams = new List<Team>();
+    }
+}

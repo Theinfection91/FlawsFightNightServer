129fe3a4a554395ba0ef323d81a258a1438a680c
Chase Carter <149976513+Theinfection91@users.noreply.github.com>
2025-09-07 18:12:02 -0400

    Super fast to get slash commands going. May not go with SQL db at first, may just use json for attempt at js front end bot with C# asp net api backend.

--- bot/index.js
+++ bot/index.js
@@ -1,7 +1,37 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+import { Client, Events, GatewayIntentBits, REST, Routes } from 'discord.js';
+import { CLIENT_ID, GUILD_ID, TOKEN } from './creds.js';
 
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on(Events.ClientReady, readyClient => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
 });
 
-client.login("YOUR_BOT_TOKEN");
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
